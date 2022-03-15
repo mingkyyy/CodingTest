b179772c739dda20db9bcb410b663edc615373f4
mingkyy <alsrus1227@naver.com>
2022-03-15 13:55:00 +0900

    최대공약수와 최소공배수

---
+++ src/math1/Ex2609.java
@@ -0,0 +1,32 @@
+package math1;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Ex2609 {
+    public static int GCD(int a, int b) {
+        if (a % b == 0) {
+            return b;
+        } else {
+            return GCD(b, a % b);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        int c = Math.max(a, b);
+        b = Math.min(a, b);
+        a = c;
+
+        int result= GCD(a,b);
+        System.out.println(result);
+        System.out.println(result * a/result * b/result);
+    }
+}
+
