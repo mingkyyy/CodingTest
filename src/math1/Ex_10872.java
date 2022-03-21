e68b737e6cff629d96f2e63c78e038072ae2de89
mingkyy <alsrus1227@naver.com>
2022-03-21 16:30:43 +0900

    팩토리얼

---
+++ src/math1/Ex_10872.java
@@ -0,0 +1,20 @@
+package math1;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Ex_10872 {
+    public static int factorial(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        System.out.println(factorial(n));
+    }
+}
