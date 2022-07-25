4998343cf71b84ab58751dc9db457de456702cd4
mingkyy <alsrus1227@naver.com>
2022-07-25 19:13:42 +0900

    연속부분 수열

---
+++ src/section3/Ex4.java
@@ -0,0 +1,33 @@
+package section3;
+
+import java.util.Scanner;
+
+public class Ex4 {
+    public int solution(int n, int m, int[] arr) {
+        int answer = 0, sum = 0, lt = 0;
+
+        for (int rt = 0; rt<n; rt++){
+            sum+=arr[rt];
+            if (sum == m ) answer++;
+            while (sum>=m){
+                sum-=arr[lt++];
+                if (sum == m) answer++;
+            }
+        }
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Ex4 T = new Ex4();
+        System.out.println(T.solution(n, m, arr));
+    }
+}
